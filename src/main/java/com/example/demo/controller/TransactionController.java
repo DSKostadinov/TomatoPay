0624007311e1b96827f21a5fb6ee404715f1cf68
Damyan <damyan.kostadinov@gmail.com>
2021-10-08 14:57:04 +0100

    clean-up

--- src/main/java/com/example/demo/controller/TransactionController.java
+++ src/main/java/com/example/demo/controller/TransactionController.java
@@ -70,8 +70,8 @@ public class TransactionController {
     public Transaction updateTransaction(@PathVariable UUID transactionId, @RequestParam String newDesc) {
         Transaction transaction = transactionService.findById(transactionId);
         transaction.setDescription(newDesc);
-        
-        return transactionService.save(transaction);;
+
+        return transactionService.save(transaction);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
